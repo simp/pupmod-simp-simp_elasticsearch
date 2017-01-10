172c2c32711217e073a1b79386e99781d6f1ce68
Nick Markowski <nmarkowski@keywcorp.com>
2017-01-10 17:54:28 -0500

    Pki quickfix

--- manifests/pki.pp
+++ manifests/pki.pp
@@ -38,7 +38,7 @@
 #
 class simp_elasticsearch::pki(
   Variant[Boolean,Enum['simp']] $pki                     = simplib::lookup('simp_options::pki' , { 'default_value' => false }),
-  Stdlib::Absolutepath          $app_pki_external_source = simplib::lookup('simp_options::pki::source', { 'default_value' => '/etc/simp/pki' }),
+  Stdlib::Absolutepath          $app_pki_external_source = simplib::lookup('simp_options::pki::source', { 'default_value' => '/etc/pki/simp' }),
   Stdlib::AbsolutePath          $app_pki_dir             = '/etc/pki/simp_apps/simp_elasticsearch/pki',
   Stdlib::AbsolutePath          $app_pki_cert            = "${app_pki_dir}/public/${facts['fqdn']}.pub",
   Stdlib::AbsolutePath          $app_pki_key             = "${app_pki_dir}/private/${facts['fqdn']}.pem",
