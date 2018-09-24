8dbcc94e07bd730fbcaf227aa25918e40e05b00a
Nick Markowski <nicholas.markowski@onyxpoint.com>
2018-09-24 09:08:24 -0400

    (SIMP-5308) updated $app_pki_external_source type (#33)
    - Updated $app_pki_external_source to accept any string. This matches the
      functionality of pki::copy.
    
    SIMP-5296 #comment Updated simp_elasticsearch
    SIMP-5308 #close

--- manifests/pki.pp
+++ manifests/pki.pp
@@ -44,7 +44,7 @@
 #
 class simp_elasticsearch::pki(
   Variant[Boolean,Enum['simp']] $pki                     = simplib::lookup('simp_options::pki' , { 'default_value' => false }),
-  Stdlib::Absolutepath          $app_pki_external_source = simplib::lookup('simp_options::pki::source', { 'default_value' => '/etc/pki/simp/x509' }),
+  String                        $app_pki_external_source = simplib::lookup('simp_options::pki::source', { 'default_value' => '/etc/pki/simp/x509' }),
   Stdlib::AbsolutePath          $app_pki_dir             = '/etc/pki/simp_apps/simp_elasticsearch/x509',
   Stdlib::AbsolutePath          $app_pki_cert            = "${app_pki_dir}/public/${facts['fqdn']}.pub",
   Stdlib::AbsolutePath          $app_pki_key             = "${app_pki_dir}/private/${facts['fqdn']}.pem",
